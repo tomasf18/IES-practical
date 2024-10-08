0dd8940afe6df94673ff7f71b1ea815467f79327
Tomás Santos <t.santos.f@ua.pt>
2024-10-08 15:15:51 +0100

    lab2_1

---
+++ Lab02/lab2_1/first-servlet-server/src/main/java/ex1/lab2/ies/deti/ua/EmbeddedJettyExample.java
@@ -0,0 +1,45 @@
+package ex1.lab2.ies.deti.ua;
+
+import java.io.IOException;
+ 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.eclipse.jetty.server.Connector;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.servlet.ServletHandler;
+ 
+public class EmbeddedJettyExample {
+ 
+    public static void main(String[] args) throws Exception {
+         
+        Server server = new Server(8680);       
+         
+        ServletHandler servletHandler = new ServletHandler();
+        server.setHandler(servletHandler);
+                 
+        servletHandler.addServletWithMapping(HelloServlet.class, "/");
+         
+        server.start();
+        server.join();
+ 
+    }
+     
+    public static class HelloServlet extends HttpServlet {
+        protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+            response.setContentType("text/html");
+            response.setStatus(HttpServletResponse.SC_OK);
+
+            String msg = request.getParameter("msg");
+
+            if (msg == null || msg.isEmpty()) {
+                msg = "Hello World!";
+            } 
+
+            response.getWriter().println("<h1>" + msg + "</h1>"); 
+        } 
+    }
+ }
